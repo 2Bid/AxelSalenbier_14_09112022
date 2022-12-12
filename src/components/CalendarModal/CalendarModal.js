ed0b3b67cac8cf3cf0b14bbee93b8a9d5f4651a7
2Bid <salenbieraxel@gmail.com>
2022-12-12 01:03:55 +0100

    mise a jour du premier du mois selon le jour ou il commence

--- src/components/CalendarModal/CalendarModal.js
+++ src/components/CalendarModal/CalendarModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getcurrentMonth, getcurrentYear } from '../../services/getDate'
+import { getAllMonth, getCurrentDay, getCurrentIndexMonth, getCurrentYear, getFirstDayMonth, getLastDayMonth, getNumberDaysMonth } from '../../services/getDate'
 
 import "./calendarModal.css"
 
@@ -7,25 +7,55 @@ import Homelogo from "../../home.png"
 
 export default function CalendarModal() {
 
-  const month = getcurrentMonth()
-  const year = getcurrentYear()
+  //
+  // STATE
+  //
 
-  const [currentDay, setCurrentDay] = useState("")
-  const [currentMonth, setCurrentMonth] = useState("")
-  const [currentYear, setCurrentYear] = useState("")
+  const day = getCurrentDay()
+  const year = getCurrentYear()
 
-  const [currentDate, setCurrentDate] = useState("")
+  const [currentDay, setCurrentDay] = useState(day)
+  const [currentYear, setCurrentYear] = useState(year)
+
+  const allMonth = getAllMonth()
+  const currentIndexMonth = getCurrentIndexMonth()
+  const [indexMonth, setIndexMonth] = useState(currentIndexMonth)
+
+  const [allDaysMonth, setAllDaysMonth] = useState([])
+  const allDays = getNumberDaysMonth(currentIndexMonth, currentYear) + 1
+
+  const firstDayMonth = getFirstDayMonth().getDay()
+  const lastDayMonth = getLastDayMonth().getDay()
+
+  const [dayFirstDayMonth, setDayFirstDayMonth] = useState(firstDayMonth)
+  const [dayLastDayMonth, setDayLastDayMonth] = useState(lastDayMonth)
+
+  // MOIS PRECEDENT ET SUIVANT
+  const [prevDaysMonth, setPrevDaysMonth] = useState([])
+  const [nextDaysMonth, setNextDaysMonth] = useState([])
+
+
+  //
+  // FONCTIONS & USEEFFECT
+  //
+
+  // mise a jour du tableau comportant les jours du mois actuel
+  useEffect(()=>{
+    for (let i = 1; i < allDays + 1; i++) {
+      setAllDaysMonth([...allDaysMonth, allDaysMonth.push(i)])
+    }
+  },[indexMonth])
 
   useEffect(()=>{
-    setCurrentMonth(month)
-    setCurrentYear(year)
-  },[])
+    for (let i = 0; i < dayFirstDayMonth; i++) {
+      
+    }
+  },[dayFirstDayMonth])
 
-  {/* 
+  useEffect(()=>{
 
-    Recuperer l'infos sur le jour du premier mois
+  },[dayLastDayMonth])
 
-  */}
 
   return (
     <div className='calendarModal'>
@@ -37,7 +67,11 @@ export default function CalendarModal() {
         </div>
 
         <select className='calendarModal__nav--month'>
-          <option defaultValue={currentMonth}>{currentMonth}</option>
+          {allMonth.map((month, index)=>{
+            return (
+              <option key={index} value={month} selected={index === currentIndexMonth}>{month}</option>
+            )
+          })}
         </select>
 
         <select className='calendarModal__nav--year'>
@@ -50,17 +84,57 @@ export default function CalendarModal() {
       <table className='calendarModal__table'>
         <thead className='calendarModal__table-head'>
           <tr>
-            <th data-day="Sun">Sun</th>
-            <th data-day="Mon">Mon</th>
-            <th data-day="Tue">Tue</th>
-            <th data-day="Wed">Wed</th>
-            <th data-day="Thu">Thu</th>
-            <th data-day="Fri">Fri</th>
-            <th data-day="Sat">Sat</th>
+            <th className='calendarModal__day--head' data-day="Sun">Sun</th>
+            <th className='calendarModal__day--head' data-day="Mon">Mon</th>
+            <th className='calendarModal__day--head' data-day="Tue">Tue</th>
+            <th className='calendarModal__day--head' data-day="Wed">Wed</th>
+            <th className='calendarModal__day--head' data-day="Thu">Thu</th>
+            <th className='calendarModal__day--head' data-day="Fri">Fri</th>
+            <th className='calendarModal__day--head' data-day="Sat">Sat</th>
           </tr>
         </thead>
 
         <tbody className='calendarModal__table-body'>
+          <tr className='calendarModal__week-1 calendarModal__week'>
+            {
+              allDaysMonth.slice(0,firstDayMonth).map((day)=>{
+                return <td key={day} className={`calendarModal__day--prev calendarModal__day ${day}`}>0</td>
+              })
+            }
+            {
+              allDaysMonth.slice(firstDayMonth,7).map((day, index)=>{
+                return <td key={day} className={`calendarModal__day ${day} ${day === currentDay ? 'active' : ''}`}>{allDaysMonth[index]}</td>
+              })
+            }
+          </tr>
+          <tr className='calendarModal__week-2 calendarModal__week'>
+            {
+              allDaysMonth.slice(7 - firstDayMonth, 14 - firstDayMonth).map((day)=>{
+                return <td key={day} className={`calendarModal__day ${day} ${day === currentDay ? 'active' : ''}`}>{day}</td>
+              })
+            }
+          </tr>
+          <tr className='calendarModal__week-3 calendarModal__week'>
+            {
+              allDaysMonth.slice(14 - firstDayMonth, 21 - firstDayMonth).map((day)=>{
+                return <td key={day} className={`calendarModal__day ${day} ${day === currentDay ? 'active' : ''}`}>{day}</td>
+              })
+            }
+          </tr>
+          <tr className='calendarModal__week-4 calendarModal__week'>
+            {
+              allDaysMonth.slice(21 - firstDayMonth, 28 - firstDayMonth).map((day)=>{
+                return <td key={day} className={`calendarModal__day ${day} ${day === currentDay ? 'active' : ''}`}>{day}</td>
+              })
+            }
+          </tr>
+          <tr className='calendarModal__week-5 calendarModal__week'>
+            {
+              allDaysMonth.slice(28 - firstDayMonth, allDaysMonth.length).map((day)=>{
+                return <td key={day} className={`calendarModal__day ${day} ${day === currentDay ? 'active' : ''}`}>{day}</td>
+              })
+            }
+          </tr>
         </tbody>
       </table>
     </div>
--- src/services/getDate.js
+++ src/services/getDate.js
@@ -1,16 +1,40 @@
-function getcurrentDate(){
-    const day = new Date().getDate()
-    console.log(day)
+function getFirstDayMonth(){
+    const date = new Date()
+    return new Date(date.getFullYear(), date.getMonth(), 1);
 }
 
-const getcurrentMonth = () =>{
-    const monthWord = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    const monthLetter = new Date()
-    return monthWord[monthLetter.getMonth()]
+function getLastDayMonth(){
+    const date = new Date()
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0);
 }
 
-const getcurrentYear = () => {
+function getCurrentDay(){
+    return new Date().getDate()
+}
+
+const getNumberDaysMonth = (year, month) => {
+    return new Date(year, month, 0).getDate();
+}
+
+const getAllMonth = () => {
+    const allMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    return allMonth
+}
+
+// const getCurrentMonth = () =>{
+//     const monthWord = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+//     const monthLetter = new Date()
+//     return monthWord[monthLetter.getMonth()]
+// }
+
+const getCurrentIndexMonth = () => {
+    const date = new Date()
+    const currentIndexMonth = date.getMonth()
+    return currentIndexMonth
+}
+
+const getCurrentYear = () => {
     return new Date().getFullYear()
 }
 
-export {getcurrentMonth, getcurrentYear}
+export {getFirstDayMonth, getLastDayMonth, getCurrentDay, getNumberDaysMonth, getAllMonth, getCurrentIndexMonth, getCurrentYear}
